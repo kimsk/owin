021dea07d616f696aa669ea6431d09bd205dad8a
Karlkim Suwamongkol <contact@karlk.im>
2014-08-24 18:33:58 -0500

    install-package microsoft.owin.host.listener

--- src/asp-net-mvc5-fundamental/KatanaIntro/KatanaIntro/Program.cs
+++ src/asp-net-mvc5-fundamental/KatanaIntro/KatanaIntro/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
 
 namespace KatanaIntro
 {
@@ -10,6 +13,21 @@ namespace KatanaIntro
     {
         static void Main(string[] args)
         {
+            string uri = "http://localhost:11112";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopped!");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx => ctx.Response.WriteAsync("Hello World!"));
         }
     }
 }
